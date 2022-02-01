822f659bd4960ad594d9bc83547205bf6b5d7b6b
Nicoletr <nicole.trpevski@gmail.com>
2022-02-01 19:22:20 +0800

    Added reducers and actions to clientside

---
+++ client/src/utils/actions.js
@@ -0,0 +1,71 @@
+/*
+ * ADD_USER:
+ * - takes a user object as payload with parameters
+ * - creates new ID for user based on previous last user
+ * - pushes new user to the end of copy of users array
+ * - updates users array
+ */
+export const ADD_USER = "ADD_USER";
+
+/*
+ * ADD_ACTIVITY:
+ * - takes an activity object as payload with parameters
+ * - creates new ID for activity based on previous last activity
+ * - pushes new activity to the end of copy of activities array
+ * - updates activities array
+ */
+export const ADD_ACTIVITY = "ADD_ACTIVITY";
+
+/*
+ * ADD_MEAL:
+ * - takes a meal object as payload with parameters
+ * - creates new ID for meal based on previous last meal
+ * - pushes new meal to the end of copy of meals array
+ * - updates meals array
+ */
+export const ADD_MEAL = "ADD_MEAL";
+
+/*
+ * UPDATE_USER:
+ * - takes object with id and updated values for user parameter(s)
+ * - finds user based on ID and updates object
+ * - updates object in-place within copy of users array
+ * - updates users array
+ */
+export const UPDATE_USER = "UPDATE_USER";
+
+/*
+ * UPDATE_ACTIVITY:
+ * - takes object with id and updated values for activity parameter(s)
+ * - finds activity based on ID and updates object
+ * - updates object in-place within copy of activities array
+ * - updates activities array
+ */
+export const UPDATE_ACTIVITY = "UPDATE_ACTIVITY";
+
+/*
+ * UPDATE_MEAL:
+ * - takes object with id and updated values for meal parameter(s)
+ * - finds meal based on ID and updates object
+ * - updates object in-place within copy of meals array
+ * - updates meals array
+ */
+export const UPDATE_MEAL = "UPDATE_MEAL";
+
+/*
+ * REMOVE_ACTIVITY:
+ * - takes a string of activity to be removed
+ * - creates copy of activities array
+ * - filters new copy to only return non-matching (to parameter) activities
+ * - updates activities array
+ */
+export const REMOVE_ACTIVITY = "REMOVE_ACTIVITY";
+
+/*
+ * REMOVE_MEAL:
+ * - takes a string of meal to be removed
+ * - creates copy of meals array
+ * - filters new copy to only return non-matching (to parameter) meals
+ * - updates meals array
+ */
+export const REMOVE_MEAL = "REMOVE_MEAL";
---
+++ client/src/utils/reducers.js
@@ -0,0 +1,121 @@
+// Import all actions
+import {
+  ADD_USER,
+  ADD_ACTIVITY,
+  ADD_MEAL,
+  UPDATE_USER,
+  UPDATE_ACTIVITY,
+  UPDATE_MEAL,
+  REMOVE_ACTIVITY,
+  REMOVE_MEAL,
+} from "./actions";
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case ADD_USER: {
+      const newUserId = state.users[state.users.length - 1].id + 1;
+
+      const newUser = { ...action.payload, id: newUserId };
+
+      return {
+        ...state,
+        users: [...state.users, newUser],
+      };
+    }
+    // TODO: all actions below need to be updated to reflect against respective logged in users
+    case ADD_ACTIVITY: {
+      const newActivityId =
+        state.activities[state.activities.length - 1].id + 1;
+
+      const newActivity = { ...action.payload, id: newActivityId };
+
+      return {
+        ...state,
+        activities: [...state.activities, newActivity],
+      };
+    }
+    case ADD_MEAL: {
+      const newMealId = state.meals[state.meals.length - 1].id + 1;
+
+      const newMeal = { ...action.payload, id: newMealId };
+
+      return {
+        ...state,
+        meals: [...state.meals, newMeal],
+      };
+    }
+    case UPDATE_USER: {
+      const userIndex = state.users.findIndex(
+        (user) => user.id === action.payload.id
+      );
+
+      const updatedUser = {
+        ...state.users[userIndex],
+        ...action.payload,
+      };
+
+      const newUsersList = [...state.users];
+
+      newUsersList[userIndex] = updatedUser;
+
+      return {
+        ...state,
+        users: newUsersList,
+      };
+    }
+    case UPDATE_ACTIVITY: {
+      const activityIndex = state.activities.findIndex(
+        (activity) => activity.id === action.payload.id
+      );
+
+      const updatedActivity = {
+        ...state.activities[activityIndex],
+        ...action.payload,
+      };
+
+      const newActivitiesList = [...state.activities];
+
+      newActivitiesList[activityIndex] = updatedActivity;
+
+      return {
+        ...state,
+        users: newActivitiesList,
+      };
+    }
+    case UPDATE_MEAL: {
+      const mealIndex = state.meals.findIndex(
+        (meal) => meal.id === action.payload.id
+      );
+
+      const updatedMeal = {
+        ...state.meals[mealIndex],
+        ...action.payload,
+      };
+
+      const newMealsList = [...state.meals];
+
+      newMealsList[mealIndex] = updatedMeal;
+
+      return {
+        ...state,
+        users: newMealsList,
+      };
+    }
+    case REMOVE_ACTIVITY: {
+      return {
+        ...state,
+        activities: [...state.activities].filter(
+          (activity) => activity.id !== action.payload
+        ),
+      };
+    }
+    case REMOVE_MEAL: {
+      return {
+        ...state,
+        meals: [...state.meals].filter((meal) => meal.id !== action.payload),
+      };
+    }
+    default:
+      return state;
+  }
+}
