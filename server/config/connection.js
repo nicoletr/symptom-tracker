444744194f03c4f7d54af9fc61c83b293df90396
Nicoletr <nicole.trpevski@gmail.com>
2022-02-08 19:30:06 +0800

    Heroku key

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/symptom-tracker",
+  (process.env.MONGODB_URI =
+    "mongodb+srv://nicoletr:Blackjack138@cluster0.0m49i.mongodb.net/symptom-tracker?retryWrites=true&w=majority" ||
+    "mongodb://localhost/symptom-tracker"),
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
